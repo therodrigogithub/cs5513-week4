4bc11e544066bcace9099a37fbead475fdc618e9
Rodrigo A <1rodrigo1a@gmail.com>
2024-09-30 22:30:59 -0700

    radded back name property to [id] pages

--- pages/[id].js
+++ pages/[id].js
@@ -29,7 +29,7 @@ export default function Entry( { itemData } ) {
     <Layout>
       <article className="card col-6">
         <div className="card-body">
-
+          <h5 className="card-title">{itemData.name}</h5>
           <h6 className="card-subtitle mb-2 text-body-secondary">{itemData.phone}</h6>
           <p className="card-text">{itemData.birthdate}</p>
           <a href="#" className="card-link">{itemData.email}</a>
